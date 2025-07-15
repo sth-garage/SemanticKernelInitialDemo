ebd46b1142be9ff0b2d5e90728d5af5e25dcaa23
sth-garage <sholt1234@gmail.com>
2025-07-15 09:37:49 -0400

    Adding plugins

--- SemanticKernelInitialDemo/Program.cs
+++ SemanticKernelInitialDemo/Program.cs
@@ -7,18 +7,26 @@ using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Connectors.Google;
 using Microsoft.SemanticKernel.Plugins.Memory;
 using Microsoft.KernelMemory;
+using SemanticKernelInitialDemo.Plugins;
+using Microsoft.SemanticKernel.Plugins.Core;
 #pragma warning disable SKEXP0010
 #pragma warning disable SKEXP0001
 
 // Populate values from your OpenAI deployment
-var modelId = "google/gemma-3-1b";
+var modelId = "qwen/qwen3-1.7b";
 var apiUrl = @"http://127.0.0.1:1234/v1/";
+
+ConsoleHelper.SetCurrentFont("Consolas", 22);
 
 
 var builder = Kernel.CreateBuilder().AddOpenAIChatCompletion(
 modelId: modelId,
 apiKey: modelId,
 endpoint: new Uri(apiUrl));
+
+builder.Plugins.AddFromType<LightsPlugin>();
+builder.Plugins.AddFromType<ExportPlugin>();
+builder.Plugins.AddFromType<TimePlugin>();
 
 
 // Build the kernel
