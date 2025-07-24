eab7207b1976a36adf3e9d67fa185b77e3c98804
sth-garage <sholt1234@gmail.com>
2025-07-24 08:34:55 -0400

    Adding agents

--- SemanticKernelInitialDemo/Program.cs
+++ SemanticKernelInitialDemo/Program.cs
@@ -1,50 +1,42 @@
 ﻿// Import packages
+using Agents;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.KernelMemory;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Connectors.Google;
-using Microsoft.SemanticKernel.Plugins.Memory;
-using Microsoft.KernelMemory;
-using SemanticKernelInitialDemo.Plugins;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Plugins.Core;
-using Agents;
+using Microsoft.SemanticKernel.Plugins.Memory;
+using OpenAI.Images;
+using SemanticKernelInitialDemo.Plugins;
 #pragma warning disable SKEXP0010
 #pragma warning disable SKEXP0001
 
 ConsoleHelper.SetCurrentFont("Consolas", 22);
 
-
-
-
-// Populate values from your OpenAI deployment
-var modelId = "google/gemma-3-4b";
-var apiUrl = @"http://127.0.0.1:1234/v1/";
+var configBuilder = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+var apiKey = configBuilder["AI:ApiKey"];
+var modelId = configBuilder["AI:Model"];
+var apiUrl = configBuilder["AI:ApiUrl"];
 
 var builder = Kernel.CreateBuilder().AddOpenAIChatCompletion(
-modelId: modelId,
-apiKey: modelId,
-endpoint: new Uri(apiUrl)).AddOpenAIChatClient(modelId: modelId);
-
-
-
-
+    modelId: modelId,
+    apiKey: apiKey,
+    endpoint: new Uri(apiUrl)
+).AddOpenAIChatClient(modelId);
 
 builder.Plugins.AddFromType<LightsPlugin>();
 builder.Plugins.AddFromType<ExportPlugin>();
 builder.Plugins.AddFromType<TimePlugin>();
 
-
 // Build the kernel
 Kernel kernel = builder.Build();
 
-
-
 MixedChat_Agents mixedChat = new MixedChat_Agents();
-await mixedChat.ChatWithOpenAIAssistantAgentAndChatCompletionAgent(kernel);
-
-
+await mixedChat.ChatWithOpenAIAssistantAgentAndChatCompletionAgent(kernel, apiKey, modelId);
 
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
@@ -80,7 +72,3 @@ do
     // Add the message from the agent to the chat history
     history.AddMessage(result.Role, result.Content ?? string.Empty);
 } while (userInput is not null);
-
-
-
-//ChatWithOpenAIAssistantAgentAndChatCompletionAgent
